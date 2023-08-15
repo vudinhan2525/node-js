5b22c5525877b9875be7c75eb145d24da604c045
vudinhan2525 <vudinhan000@gmail.com>
2023-08-15 21:21:50 +0700

    nt

--- 4-Natours/controller/authController.js
+++ 4-Natours/controller/authController.js
@@ -1,8 +1,15 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 
 exports.signup = catchAsync(async (req, res, next) => {
-    const newUser = await User.create(req.body);
+    const newUser = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        passwordConfirm: req.body.passwordConfirm,
+    });
     res.status(200).json({
         status: 'success',
         data: newUser,
