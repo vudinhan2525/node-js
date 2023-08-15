e671544ccf7b8a4ab322e705a05ec4c965b4b2f0
vudinhan2525 <vudinhan000@gmail.com>
2023-08-15 17:26:09 +0700

    nt

--- 4-Natours/models/userModel.js
+++ 4-Natours/models/userModel.js
@@ -2,6 +2,9 @@ const validator = require('validator');
 
 const mongoose = require('mongoose');
 
+// eslint-disable-next-line import/no-extraneous-dependencies
+const bcrypt = require('bcryptjs');
+
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -27,10 +30,23 @@ const userSchema = new mongoose.Schema({
     passwordConfirm: {
         type: String,
         required: [true, 'User must have a confirm password'],
+        validate: {
+            validator: function (val) {
+                return val === this.password;
+            },
+            message: 'Password confirm not true',
+        },
     },
     photo: {
         type: String,
     },
+});
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+
+    this.password = await bcrypt.hash(this.password, 12);
+    this.passwordConfirm = undefined;
+    next();
 });
 const User = mongoose.model('User', userSchema);
 module.exports = User;
